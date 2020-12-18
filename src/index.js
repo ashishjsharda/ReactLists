c3bc45e123a654e4dda57ef63c0020f746084090
Ashish Sharda <ashishjsharda@gmail.com>
2020-12-18 06:17:33 -0500

    Add all my files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+const fruits=["Apple","Banana","Grapes"];
+const element=React.createElement('ol',null,
+fruits.map((fruit)=>React.createElement('li',{key:fruit},fruit)));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    element,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
